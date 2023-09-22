8617672a2d633688d3dac1a023765a5579da4e2c
Leela210 <144507605+Leela210@users.noreply.github.com>
2023-09-22 17:31:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-num=int(input("please enter the number your wish:"))
-if(num%4==0):
-  if(num%100==0):
-    if(num%400==0):
-      print("%d is a leap year"%num) 
-    else:
-      print("%d is not"%num)
-  else:
-    print("%d ia a leap year"%num)
-else:
-  print("%d is not"%num)
-  
+def linearSearchProduct(productList,  targetproduct):
+  indices = []
+  for index, product in enumerate(productList):
+    if product == targetproduct:
+      indices.append(index)
+  return indices
+products=["shoes","boot","loafer","shoes", "sandal","shoes"]
+target="shoes"
+target2="apple"
+result=linearSearchProduct(products, target)
+print(result)
